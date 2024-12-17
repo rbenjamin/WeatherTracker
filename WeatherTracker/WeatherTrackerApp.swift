38a0d3e739b3f3c2288422e2c590522922999159
Ben Davis <ben@pinwheeldevinc.com>
2024-12-17 10:28:51 -0500

    Initial Commit

---
+++ WeatherTracker/WeatherTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTrackerApp.swift
+//  WeatherTracker
+//
+//  Created by Ben Davis on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
